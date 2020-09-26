26b6bd38ea51b7e0c8d382f47a825f70f228d59e
phineas1123 <muhammadimamt@gmail.com>
2020-09-26 19:27:01 +0700

    add scroll top fab

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,3 +1,22 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#scroll-top").fadeIn();
+    } else {
+      $("#scroll-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#scroll-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
 function scrollClick(id) {
   console.log("pressed");
   $("html,body").animate(
